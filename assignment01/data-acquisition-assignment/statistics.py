90fd0e3fa2c18b21b374ec8ea0d73fadca825f1f
Adnan Manzoor <adnan904@gmail.com>
2020-05-02 15:32:04 +0200

    Adding Preliminary statistics

--- assignment01/data-acquisition-assignment/statistics.py
+++ assignment01/data-acquisition-assignment/statistics.py
@@ -1,6 +1,6 @@
 import spacy
-import pandas as pd
 import os
+import json
 import timeit
 
 #############################################
@@ -22,7 +22,7 @@ def get_train_split() -> list:
         file_content = train_file.read().split('\n')[1:]
         for line in file_content:
             if 'TRAIN' in line:
-                train_ids.append(int(line.split('";')[0].split('"essay')[1]))
+                train_ids.append(line.split('";')[0].split('"essay')[1])
         return train_ids
 
 
@@ -55,41 +55,56 @@ def main():
     train_split_essay_ids = get_train_split()
     # Number of Essays = Number of Essays in the train-test-split.csv file that have been SET 'TRAIN'
     num_of_essays = len(train_split_essay_ids)
-    with open(UNIFIED_DATA_FILE_PATH, 'r') as data_file:
-        # Creating a Pandas Dataframe from the unified data json file
-        df = pd.read_json(data_file)
-        for essay in df.values:
+
+    with open(UNIFIED_DATA_FILE_PATH, 'r') as f:
+        unified_file = json.load(f)
+        for essay in unified_file:
             # We only need to compute for essays SET to 'TRAIN'
-            if essay[0] in train_split_essay_ids:
+            if essay['id'] in train_split_essay_ids:
                 # Tokenizing the text for the essay using the spaCy library
-                text = nlp(essay[2])
-                num_of_paragraphs += len(essay[6])
+                text = nlp(essay['text'])
+                num_of_paragraphs += len(essay['paragraphs'])
                 # Using the spaCy library for calculating Sentences in the text
                 num_of_sentences += len(list(text.sents))
                 num_of_tokens += len(text)
-                num_of_major_claims += len(essay[3])
-                num_of_claims += len(essay[4])
-                num_of_premises += len(essay[5])
-                if essay[7]:
+                num_of_major_claims += len(essay['major_claim'])
+                num_of_claims += len(essay['claims'])
+                num_of_premises += len(essay['premises'])
+                if essay['confirmation_bias']:
                     num_of_essays_with_conf_bias += 1
                 else:
                     num_of_essays_without_conf_bias += 1
-                for para in essay[6]:
+                for para in essay['paragraphs']:
                     if para['sufficient']:
                         num_of_suff_paras += 1
                     else:
                         num_of_insuff_paras += 1
                 # Tokenizing using the nlp() of the spaCy library
-                for major_claim in essay[3]:
+                for major_claim in essay['major_claim']:
                     num_of_tokens_in_major_claims += len(nlp(major_claim['text']))
-                for claim in essay[4]:
+                for claim in essay['claims']:
                     num_of_tokens_in_claims += len(nlp(claim['text']))
-                for premise in essay[5]:
+                for premise in essay['premises']:
                     num_of_tokens_in_premises += len(nlp(premise['text']))
         avg_num_of_tokens_in_major_claims = num_of_tokens_in_major_claims / num_of_major_claims
         avg_num_of_tokens_in_claims = num_of_tokens_in_claims / num_of_claims
         avg_num_of_tokens_in_premises = num_of_tokens_in_premises / num_of_premises
 
+    print("The Preliminary Statistics are:")
+    print("Number of essays: " + str(num_of_essays))
+    print("Number of paragraphs: " + str(num_of_paragraphs))
+    print("Number of sentences: " + str(num_of_sentences))
+    print("Number of tokens: " + str(num_of_tokens))
+    print("Number of major claims: " + str(num_of_major_claims))
+    print("Number of claims: " + str(num_of_claims))
+    print("Number of premises: " + str(num_of_premises))
+    print("Number of essays with confirmation bias: " + str(num_of_essays_with_conf_bias))
+    print("Number of essays without confirmation bias: " + str(num_of_essays_without_conf_bias))
+    print("Number of sufficient paragraphs: " + str(num_of_suff_paras))
+    print("Number of insufficient paragraphs: " + str(num_of_insuff_paras))
+    print("Average number of tokens in major claims: " + str(avg_num_of_tokens_in_major_claims))
+    print("Average number of tokens in claims: " + str(avg_num_of_tokens_in_claims))
+    print("Average number of tokens in premises: " + str(avg_num_of_tokens_in_premises))
     stop = timeit.default_timer()
     print('Time: ', stop - start)
 
