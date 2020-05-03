85239d0d4f4a1cc9fef3d143068adb3a30fb2395
Adnan Manzoor <adnan904@gmail.com>
2020-05-03 12:58:36 +0200

    Improving the run-time a little bit using a dict

--- assignment01/data-acquisition-assignment/statistics.py
+++ assignment01/data-acquisition-assignment/statistics.py
@@ -19,9 +19,9 @@ nlp = spacy.load("en_core_web_sm")
 
 def get_train_test_split_dict_and_num_essays():
     """
-    Reads the train-test-split.csv file and returns a dict {essayid : split} and number of essays set as TRAIN
+    Reads the train-test-split.csv file and returns a dict {'essayid' : 'split'} and number of essays set as TRAIN
     :return: num_essays : number od essays SET to TRAIN in the train-test-split.csv
-             train_test_split_dict: a dict of the form {essayid : split}
+             train_test_split_dict: a dict of the form {'essayid' : 'split'}
     """
     with open(TRAIN_TEST_SPLIT_FILE_PATH, 'r') as train_file:
         train_test_split_dict = {}
@@ -36,7 +36,7 @@ def get_train_test_split_dict_and_num_essays():
         return num_essays, train_test_split_dict
 
 
-def get_most_specific_words(text_list: list) -> list:
+def get_most_common_words(text_list: list) -> list:
     """
     Gets a list of lowercase sentences. It performs the following on them:
         - Joins the sentences to a single text string
@@ -46,7 +46,7 @@ def get_most_specific_words(text_list: list) -> list:
         - Filters the 10 most common words
         - Returns a list of tuples of the form (word, frequency)
     :param text_list: a list of lowercase sentences
-    :return: a list of 10 most specific tuples of the form (word, frequency)
+    :return: a list of 10 most common tuples of the form (word, frequency)
     """
     text = '. '.join(text_list)
     tokens = nlp(text)
@@ -77,7 +77,7 @@ def main():
     claims_text = []
     premises_text = []
     # Number of Essays = Number of Essays in the train-test-split.csv file that have been SET 'TRAIN'
-    # Getting the list of IDs of train-split from train-test-split.csv
+    # Getting the dict of train-test-split of the form {'essayid' : 'split'}
     num_of_essays, train_test_split_dict = get_train_test_split_dict_and_num_essays()
 
     with open(UNIFIED_DATA_FILE_PATH, 'r') as f:
@@ -104,6 +104,7 @@ def main():
                     else:
                         num_of_insuff_paras += 1
                 # Tokenizing using the nlp() of the spaCy library
+                # Appending the text of the argument unit to a list
                 for major_claim in essay['major_claim']:
                     num_of_tokens_in_major_claims += len(nlp(major_claim['text']))
                     major_claims_text.append(major_claim['text'])
@@ -119,9 +120,9 @@ def main():
         avg_num_of_tokens_in_premises = num_of_tokens_in_premises / num_of_premises
 
         # Calculating the 10 most specific words in major_claims, claims, and premises
-        major_claims_specific_words = get_most_specific_words(major_claims_text)
-        claims_specific_words = get_most_specific_words(claims_text)
-        premises_specific_words = get_most_specific_words(premises_text)
+        major_claims_common_words = get_most_common_words(major_claims_text)
+        claims_common_words = get_most_common_words(claims_text)
+        premises_common_words = get_most_common_words(premises_text)
 
     print("The Preliminary Statistics are:")
     print("Number of essays: {}".format(num_of_essays))
@@ -139,13 +140,13 @@ def main():
     print("Average number of tokens in claims: {}".format(avg_num_of_tokens_in_claims))
     print("Average number of tokens in premises: {}".format(avg_num_of_tokens_in_premises))
     print("\n10 most specific words in major claims:")
-    for i, word in enumerate(major_claims_specific_words):
+    for i, word in enumerate(major_claims_common_words):
         print("{}) '{}' -- {} times".format(i+1, word[0], word[1]))
     print("\n10 most specific words in claims:")
-    for i, word in enumerate(claims_specific_words):
+    for i, word in enumerate(claims_common_words):
         print("{}) '{}' -- {} times".format(i+1, word[0], word[1]))
     print("\n10 most specific words in premises:")
-    for i, word in enumerate(premises_specific_words):
+    for i, word in enumerate(premises_common_words):
         print("{}) '{}' -- {} times".format(i+1, word[0], word[1]))
 
     stop = timeit.default_timer()
