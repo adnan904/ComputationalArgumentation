2fbdd218feabcb008e458786dc17259741e26e3f
Adnan Manzoor <adnan904@gmail.com>
2020-05-21 23:16:43 +0200

    Adding the Train and Test Bio files

--- assignment02/argument-mining-assignment/code/convert_to_train_test_bio.py
+++ assignment02/argument-mining-assignment/code/convert_to_train_test_bio.py
@@ -1,6 +1,7 @@
 import json
 import os
-import pandas as pd
+import csv
+import timeit
 from convert_to_bio import convert_to_bio  # make sure 'convert_to_bio.py' and the current file are in the same folder
 
 CURRENT_WORKING_DIR = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
@@ -8,50 +9,54 @@ CORPUS_PATH = f'{CURRENT_WORKING_DIR}/data/essay_corpus.json'
 SPLIT_FILE_PATH = f'{CURRENT_WORKING_DIR}/data/train-test-split.csv'
 
 
-def train_test_split(corpus, split_scheme) -> (list, list):
+def get_train_test_split_essays(corpus, split_scheme) -> (list, list):
     """
     :param corpus: unified data file with all the essays
-    :param split_scheme: train_test_slit scheme file as Panda DF
+    :param split_scheme: train_test_split scheme file
     :rtype: list, list
-    :return: tuple of list of train, test split
+    :return: lists of train, test split essay data
     """
 
-    # split essays into training and testing
-    train_data = split_scheme.loc[split_scheme['SET'] == 'TRAIN']
-    test_data = split_scheme.loc[split_scheme['SET'] == 'TEST']
+    train_test_split_dict = {}
+    all_test_data = []
+    all_train_data = []
+
+    # create a dict of the type: {essay_id: Tag},  where Tag = 'TRAIN' or 'TEST'
+    for row in split_scheme:
+        if len(row) > 0:
+            essay_id = int(row[0].split('essay')[1])
+            train_test_split_dict[essay_id] = row[1]
 
     # extract essays that match the test_train_split scheme
-    all_test_data = search_and_extract_data(corpus, test_data)
-    all_train_data = search_and_extract_data(corpus, train_data)
+    for essay in corpus:
+        if train_test_split_dict[essay['id']] == 'TRAIN':
+            all_train_data.append(essay)
+        else:
+            all_test_data.append(essay)
 
     return all_test_data, all_train_data
 
 
-def search_and_extract_data(all_json_data, split_type_data) -> list:
-    """
-    :param split_type_data: train or test essays
-    :param all_json_data:  unified data file with all the essays
-    :rtype: list
-    :return: list of extracted essays
-    """
-    extracted_data = []
-    for essay_id in split_type_data['ID']:
-        for essay in all_json_data:
-            if int(essay_id.split('essay')[1]) == essay['id']:
-                extracted_data = extracted_data + [essay]
-    return extracted_data
-
-
 if __name__ == "__main__":
+    start = timeit.default_timer()
     json_corpus = json.load(open(CORPUS_PATH))
 
-    # Read train_test_split as panda data-frame
-    train_test_split_file = pd.read_csv(SPLIT_FILE_PATH, sep=';')
-    test, train = train_test_split(json_corpus, train_test_split_file)
-    train_bio = convert_to_bio(train)
-    test_bio = convert_to_bio(test)
-    with open(f'{CURRENT_WORKING_DIR}/data/train_BIO.txt', "w") as train_file:
+    # Read train_test_split and get essays from the unified corpus based on the split
+    with open(SPLIT_FILE_PATH, newline='') as csvfile:
+        train_test_split_file = csv.reader(csvfile, delimiter=';')
+        next(train_test_split_file, None)
+        test_essays, train_essays = get_train_test_split_essays(json_corpus, train_test_split_file)
+
+    # Getting the BIO tags for each essay in the train-test split
+    train_bio = convert_to_bio(train_essays)
+    test_bio = convert_to_bio(test_essays)
+
+    # Writing the BIO tags into the Train and Test BIO text files
+    with open(f'{CURRENT_WORKING_DIR}/data/train_BIO.txt', "w") \
+            as train_file, open(f'{CURRENT_WORKING_DIR}/data/test_BIO.txt', "w") as test_file:
         train_file.write(''.join(train_bio))
-    with open(f'{CURRENT_WORKING_DIR}/data/test_BIO.txt', "w") as test_file:
         test_file.write(''.join(test_bio))
-    print("Successfully created train and test Bio in '/data/'.")
+
+    print("\nSuccessfully created train and test Bio in '/data/'.")
+    stop = timeit.default_timer()
+    print('\nTime: ', stop - start, 'sec')
