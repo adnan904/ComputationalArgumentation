b3bb044d06851b3405502536f4ec005f83de28ae
Adnan Manzoor <adnan904@gmail.com>
2020-05-28 17:45:08 +0200

    Adding Cross-validation

--- assignment02/argument-mining-assignment/code/feature_extraction.py
+++ assignment02/argument-mining-assignment/code/feature_extraction.py
@@ -1,23 +1,16 @@
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
-from gensim.models import Word2Vec
+# from gensim.models import Word2Vec
 import os
-import numpy as np
-from sklearn import linear_model
+from sklearn import linear_model, preprocessing
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.linear_model import SGDClassifier
 from sklearn.metrics import accuracy_score, f1_score
+from sklearn.model_selection import KFold, cross_val_predict
 import random
-from numpy import zeros
-
-from tensorflow import keras
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense, TimeDistributed, Bidirectional, Embedding, Flatten
 
 
 CURRENT_WORKING_DIR = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname("__file__")))
@@ -32,11 +25,10 @@ def predict(vectorizer, classifier, data):
 
 
 def get_sentences(tokens):
-    '''
-
+    """
     :param tokens: all the tokens in lowercase from a BIO file
     :return: a list of paragraphs from from the file
-    '''
+    """
     sentences_list = []
     sentence = []
     for token in tokens:
@@ -52,54 +44,70 @@ def get_sentences(tokens):
 
 
 if __name__ == '__main__':
-    train_df = pd.read_csv(TRAINING_DATA_PATH, names=['token', 'tag'], sep='\t', skipinitialspace=True, quotechar='"')
-    train_tokens = train_df['token'].replace("\t", "", regex=True).replace("\n", "", regex=True)
-    train_tokens_lowercase = np.array([x.lower() if isinstance(x, str) else x for x in train_tokens]).astype('U')
-    train_tags = train_df['tag'].astype('U')
-    test_df = pd.read_csv(TEST_DATA_PATH, names=['token', 'tag'], sep='\t', skipinitialspace=True, quotechar='"')
-    test_tokens = test_df['token']
-    test_tokens_lowercase = np.array([x.lower() if isinstance(x, str) else x for x in test_tokens]).astype('U')
-    test_tags = test_df['tag'].astype('U')
+    # reading Files
+    train_df = pd.read_csv(TRAINING_DATA_PATH, names=['token', 'tag'], sep='\t')
+    test_df = pd.read_csv(TEST_DATA_PATH, names=['token', 'tag'], sep='\t')
+
+    # getting training and testing data
+    train_X = train_df['token'].astype('U')
+    test_X = test_df['token'].astype('U')
+    encoder = preprocessing.LabelEncoder()
+    train_y = encoder.fit_transform(train_df['tag'].astype('U'))
+    test_y = encoder.fit_transform(test_df['tag'].astype('U'))
+    labels_map = {0: 'B-CLAIM', 1: 'B-MAJOR-CLAIM', 2: 'B-PREMISE', 3: 'I-CLAIM', 4: 'I-MAJOR-CLAIM', 5: 'I-PREMISE',
+                  6: 'O'}
+
     # Class Distribution. Mostly I-PREMISE and O are majority_classes
+    print("\nTotal data-points in training data: " + str(len(train_X)))
+    print("\nTotal data-points in test data: " + str(len(test_X)))
+    print("\nTraining Data Class Distribution:")
+    print(train_df.tag.value_counts())
+    print("\nTest Data Class Distribution:")
     print(test_df.tag.value_counts())
-    count_vectorizer = CountVectorizer(
-        analyzer="word", preprocessor=None, lowercase=True)
+    count_vectorizer = CountVectorizer(analyzer="word", preprocessor=None, lowercase=True)
 
     # BoW for training data
-    train_data_bag_of_words = count_vectorizer.fit_transform(train_tokens_lowercase)
-    # cv = count_vectorizer.vocabulary_
+    train_data_bag_of_words = count_vectorizer.fit_transform(train_X)
 
     # Logistic Regression for our model with bag-of-words features
     logreg = linear_model.LogisticRegression(n_jobs=4, C=1e5, max_iter=100)
-    logreg = logreg.fit(train_data_bag_of_words, train_tags)
-    y_pred = predict(count_vectorizer, logreg, test_tokens_lowercase)
-    print('LogReg accuracy: %s' % accuracy_score(y_pred, test_tags))
-    print('LogReg F1: %s' % f1_score(y_pred, test_tags, average='macro'))
+    logreg = logreg.fit(train_data_bag_of_words, train_y)
+    y_pred = predict(count_vectorizer, logreg, test_X)
+    print('LogReg accuracy: %s' % accuracy_score(y_pred, test_y))
+    print('LogReg F1-macro: %s' % f1_score(y_pred, test_y, average='macro'))
+    print('LogReg F1-weighted: %s' % f1_score(y_pred, test_y, average='weighted'))
 
+    # Majority-class
     majority_class_pred = []
-    for _ in range(len(test_tags)):
-        majority_class_pred.append('I-PREMISE')
-    print('majority-class accuracy: %s' % accuracy_score(majority_class_pred, test_tags))
-    print('majority-class F1: %s' % f1_score(majority_class_pred, test_tags, average='macro'))
+    for _ in range(len(test_y)):
+        majority_class_pred.append(5)
+    print('majority-class accuracy: %s' % accuracy_score(majority_class_pred, test_y))
+    print('majority-class F1-macro: %s' % f1_score(majority_class_pred, test_y, average='macro'))
+    print('majority-class F1-weighted: %s' % f1_score(majority_class_pred, test_y, average='weighted'))
 
+    # Random-majority class
     random_class_pred = []
-    for _ in range(len(test_tags)):
+    for _ in range(len(test_y)):
         choice = random.choice([0, 1])
         if choice == 0:
-            random_class_pred.append('I-PREMISE')
+            random_class_pred.append(5)
         else:
-            random_class_pred.append('O')
-    print('random-majority-class accuracy: %s' % accuracy_score(random_class_pred, test_tags))
-    print('random-majority-class F1: %s' % f1_score(random_class_pred, test_tags, average='macro'))
+            random_class_pred.append(6)
+    print('random-majority-class accuracy: %s' % accuracy_score(random_class_pred, test_y))
+    print('random-majority-class F1-macro: %s' % f1_score(random_class_pred, test_y, average='macro'))
+    print('random-majority-class F1-weighted: %s' % f1_score(random_class_pred, test_y, average='weighted'))
+
+    # Random-Forrest
     rf_vectorizer = CountVectorizer(analyzer="word", preprocessor=None, lowercase=True)
-    train_data = rf_vectorizer.fit_transform(train_tokens_lowercase)
+    train_data = rf_vectorizer.fit_transform(train_X)
 
-    forest = RandomForestClassifier(n_estimators=200, max_depth=15, random_state=0)
-    forest = forest.fit(train_data, train_tags)
+    forest = RandomForestClassifier(n_estimators=150, max_depth=5, random_state=0)
+    forest = forest.fit(train_data, train_y)
 
-    rf_pred = predict(rf_vectorizer, forest, test_tokens_lowercase)
-    print('random-forest accuracy: %s' % accuracy_score(rf_pred, test_tags))
-    print('random-forest F1: %s' % f1_score(rf_pred, test_tags, average='macro'))
+    rf_pred = predict(rf_vectorizer, forest, test_X)
+    print('random-forest accuracy: %s' % accuracy_score(rf_pred, test_y))
+    print('random-forest F1-macro: %s' % f1_score(rf_pred, test_y, average='macro'))
+    print('random-forest F1-weighted: %s' % f1_score(rf_pred, test_y, average='weighted'))
 
     # Naive-BAyes
     nb = Pipeline([('vect', CountVectorizer()),
@@ -107,71 +115,44 @@ if __name__ == '__main__':
                    ('clf', MultinomialNB()),
                    ])
 
-    nb.fit(train_tokens_lowercase, train_tags)
-    nb_pred = nb.predict(test_tokens_lowercase)
-    print('NB accuracy: %s' % accuracy_score(nb_pred, test_tags))
-    print('NB F1: %s' % f1_score(nb_pred, test_tags, average='macro'))
+    nb.fit(train_X, train_y)
+    nb_pred = nb.predict(test_X)
+    print('NB accuracy: %s' % accuracy_score(nb_pred, test_y))
+    print('NB F1-macro: %s' % f1_score(nb_pred, test_y, average='macro'))
+    print('NB F1-weighted: %s' % f1_score(nb_pred, test_y, average='weighted'))
 
     # Linear SVM
-    sgd = Pipeline([('vect', CountVectorizer()),
-                    ('tfidf', TfidfTransformer()),
-                    ('clf',
-                     SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, random_state=42, max_iter=100, tol=None,
-                                   n_jobs=-1)),
+    sgd = Pipeline([('vect', CountVectorizer()),        # Counts occurrences of each word
+                    ('tfidf', TfidfTransformer()),      # Normalize the counts based on document length
+                    ('clf',   SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, max_iter=100, tol=None, n_jobs=-1,
+                                            random_state=9976))
                     ])
-    sgd.fit(train_tokens_lowercase, train_tags)
-    sgd_pred = sgd.predict(test_tokens_lowercase)
-    print('SVM accuracy: %s' % accuracy_score(sgd_pred, test_tags))
-    print('SVM F1: %s' % f1_score(sgd_pred, test_tags, average='macro'))
+    f1_list = []
+    acc_list = []
+    cv = KFold(n_splits=5)
+    for train_index, test_index in cv.split(train_X):
+        X_train, X_val = train_X[train_index], train_X[test_index]
+        y_train, y_val = train_y[train_index], train_y[test_index]
+        sgd.fit(X_train, y_train)
+        predicted = sgd.predict(X_val)
+        f1 = f1_score(y_val, predicted, average='macro')
+        acc = accuracy_score(y_val, predicted)
+        f1_list.append(f1)
+        acc_list.append(acc)
+    print(f1_list)
+    print(acc_list)
+    sgd_pred = sgd.predict(test_X)
+    print('SVM accuracy: %s' % accuracy_score(sgd_pred, test_y))
+    print('SVM F1-macro: %s' % f1_score(sgd_pred, test_y, average='macro'))
+    print('SVM F1-weighted: %s' % f1_score(sgd_pred, test_y,  average='weighted'))
 
     # Word-Embeddings
-    sentences = get_sentences(train_tokens_lowercase)
-    model = Word2Vec(sentences, min_count=1, size=100, window=10)
-    words = list(model.wv.vocab)
+    # sentences = get_sentences(train_tokens_lowercase)
+    # model = Word2Vec(sentences, min_count=1, size=100, window=10)
+    # words = list(model.wv.vocab)
     # access vector for one word
-    print(model['should'])
+    # print(model['should'])
     # find most_similar for 'should'
-    result = model.most_similar(positive=['should'], topn=1)
-    print(result)
+    # result = model.most_similar(positive=['should'], topn=1)
+    # print(result)
 
-
-    # prepare tokenizer 
-    t = Tokenizer()
-    t.fit_on_texts(sentences)
-    vocab_size = len(t.word_index) + 1
-    # integer encode the documents
-    encoded_docs = t.texts_to_sequences(sentences)
-    # pad documents to a max length of 4 words
-    
-
-    # get the embedding matrix from the embedding layer 
-    embedding_matrix = zeros((vocab_size, 100))
-    for word, i in t.word_index.items():
-        embedding_vector = model[word]
-    if embedding_vector is not None:
-        embedding_matrix[i] = embedding_vector
-
-    max_length = max([len(arr) for arr in sentences])
-    X = pad_sequences(encoded_docs, maxlen=max_length, padding='post')
-    
-    # main model    
-    input = keras.Input(shape=(max_length,))
-    model = Embedding(vocab_size, 100, weights=[embedding_matrix], input_length=max_length)(input)    
-    model =  Bidirectional(LSTM (100, return_sequences=False, dropout=0.50), merge_mode='concat')(model)
-    # model = TimeDistributed(Dense(100,activation='relu'))(model)
-    model = Flatten()(model)
-
-    model = Dense(100,activation='relu')(model)
-    output = Dense(3,activation='softmax')(model)
-    model = keras.Model(input,output)
-    model.compile(loss='sparse_categorical_crossentropy',optimizer='adam', metrics=['accuracy'])
-    print(model.summary())
-
-    # TODO: train_tags has to be encoded -> is it necessary to pad the input?
-    # Consequently, we also have to add a padding to the train tags
-    # model.fit(X, train_tags, validation_split=0.25,epochs= 10, verbose = 2)
-
-    # evaluate the model
-    # bilstm_pred = model.predict(test_tokens_lowercase) # .evaluate(train_df, test_df, verbose=2)
-    # print('Bi-LSTM accuracy: %s' % accuracy_score(bilstm_pred, test_tags))
-    # print('Bi-LSTM  F1: %s' % f1_score(bilstm_pred, test_tags, average='macro'))
