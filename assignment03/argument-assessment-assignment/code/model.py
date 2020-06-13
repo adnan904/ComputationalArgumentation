5229097e6994713e290ff5a28fa83256a5f7bc15
Adnan Manzoor <adnan904@gmail.com>
2020-06-13 22:28:35 +0200

    Adding assignment03 initial results

---
+++ assignment03/argument-assessment-assignment/code/model.py
@@ -0,0 +1,92 @@
+import json
+import pandas as pd
+import os
+import csv
+from sklearn.metrics import f1_score
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
+from sklearn.linear_model import SGDClassifier
+from sklearn.pipeline import Pipeline
+CURRENT_WORKING_DIR = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+CORPUS_PATH = f'{CURRENT_WORKING_DIR}/../data/unified_data.json'
+SPLIT_FILE_PATH = f'{CURRENT_WORKING_DIR}/../data/train-test-split.csv'
+
+
+def get_train_test_split_essays(corpus, split_scheme) -> (list, list):
+    """
+    :param corpus: unified data file with all the essays
+    :param split_scheme: train_test_split scheme file
+    :rtype: list, list
+    :return: lists of train, test split essay data
+    """
+
+    train_test_split_dict = {}
+    test_df = pd.DataFrame(columns=['id', 'text', 'bias'])
+    train_df = pd.DataFrame(columns=['id', 'text', 'bias'])
+
+    # create a dict of the type: {essay_id: Tag},  where Tag = 'TRAIN' or 'TEST'
+    for row in split_scheme:
+        if len(row) > 0:
+            essay_id = int(row[0].split('essay')[1])
+            train_test_split_dict[essay_id] = row[1]
+
+    # extract essays that match the test_train_split scheme
+    for essay in corpus:
+        if train_test_split_dict[int(essay['id'])] == 'TRAIN':
+            text = essay['text'].split('\n\n')
+            train_df = train_df.append({'id': essay['id'], 'text': text, 'bias': essay['confirmation_bias']},
+                                       ignore_index=True)
+        else:
+            text = essay['text'].split('\n\n')
+            test_df = test_df.append({'id': essay['id'], 'text': text, 'bias': essay['confirmation_bias']},
+                                     ignore_index=True)
+    train_df.sort_values('id', inplace=True)
+    test_df.sort_values('id', inplace=True)
+    return train_df, test_df
+
+
+if __name__ == "__main__":
+    json_corpus = json.load(open(CORPUS_PATH))
+
+    # Read train_test_split and get essays from the unified corpus based on the split
+    with open(SPLIT_FILE_PATH, newline='') as csvfile:
+        train_test_split_file = csv.reader(csvfile, delimiter=';')
+        next(train_test_split_file, None)
+        train_essays, test_essays = get_train_test_split_essays(json_corpus, train_test_split_file)
+        train_X = [x[0] for x in train_essays['text']]
+        train_y = list(train_essays['bias'])
+        test_X = [x[0] for x in test_essays['text']]
+        test_y = list(test_essays['bias'])
+
+        # Naive Bayes
+        nb_pipeline = Pipeline([('vec', TfidfVectorizer(ngram_range=(2, 3))),
+                                ('clf', MultinomialNB())
+                                ])
+
+        nb_pipeline.fit(train_X, train_y)
+        pred = nb_pipeline.predict(test_X)
+        f1 = f1_score(y_true=test_y, y_pred=pred)
+        print('F1 for Naive Bayes: ' + str(f1))
+
+        # Kernel SVM
+        svm_pipeline = Pipeline([('vec', TfidfVectorizer()),
+                                ('clf',  SVC(kernel='poly', degree=2, max_iter=1000))
+                                 ])
+
+        svm_pipeline.fit(train_X, train_y)
+        pred = svm_pipeline.predict(test_X)
+        f1 = f1_score(y_true=test_y, y_pred=pred)
+        print('F1 for poly-SVM: ' + str(f1))
+
+        # Linear SVM
+        lin_svm_pipeline = Pipeline([('vec', TfidfVectorizer(ngram_range=(2, 3))),
+                                     ('clf', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, max_iter=1000,
+                                                           tol=None, n_jobs=-1))
+                                     ])
+
+        lin_svm_pipeline.fit(train_X, train_y)
+        pred = lin_svm_pipeline.predict(test_X)
+        f1 = f1_score(y_true=test_y, y_pred=pred)
+        print('F1 for lin-SVM: ' + str(f1))
